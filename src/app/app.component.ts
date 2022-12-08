36e048462173e4790bff2302f5a092db78bdce15
arekgotfryd <arekgotfryd@g.pl>
2022-12-08 15:22:38 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FeedbackAndChatComponent } from './feedback-and-chat/feedback-and-chat.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [CommonModule, RouterModule, FeedbackAndChatComponent],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
+
+const routes = [
+  {
+    path: 'feedback-and-chat',
+    loadComponent: () => import('./app/feedback-and-chat/feedback-and-chat.component').then(c => c.FeedbackAndChatComponent),
+  }
+]
 
 if (environment.production) {
   enableProdMode();
 }
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent,{providers: [provideRouter(routes)]}).catch(error => console.error(error));
